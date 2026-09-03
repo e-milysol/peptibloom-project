4df78dc56142283409335ce65142e5ba021726fa
e-milysol <milysol2702@gmail.com>
2026-09-03 10:43:49 +0200

    governance: restore sitemap to main scope

--- apps/web/src/pages/sitemap.xml.ts
+++ apps/web/src/pages/sitemap.xml.ts
@@ -3,10 +3,10 @@ import type { APIRoute } from 'astro';
 export const prerender = true;
 
 const origin = 'https://peptibloomproject.com';
-const routes = ['/', '/about/', '/calculator/', '/library/', '/library/retatrutide/', '/methodology/'];
+const routes = ['/', '/about/', '/calculator/', '/library/', '/methodology/'];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/sitemap/0.9">
 ${routes.map((route) => `  <url><loc>${origin}${route}</loc></url>`).join('\n')}
 </urlset>`;
 

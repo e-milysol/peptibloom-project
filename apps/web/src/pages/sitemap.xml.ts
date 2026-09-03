50dcceef2afcaf903704fe4c8756ade1319581d7
e-milysol <milysol2702@gmail.com>
2026-09-03 10:14:46 +0200

    web: add retatrutide to sitemap

--- apps/web/src/pages/sitemap.xml.ts
+++ apps/web/src/pages/sitemap.xml.ts
@@ -3,7 +3,7 @@ import type { APIRoute } from 'astro';
 export const prerender = true;
 
 const origin = 'https://peptibloomproject.com';
-const routes = ['/', '/about/', '/calculator/', '/library/', '/methodology/'];
+const routes = ['/', '/about/', '/calculator/', '/library/', '/library/retatrutide/', '/methodology/'];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">

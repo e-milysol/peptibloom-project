e51c584016971e6da37f621e3998fc217e437030
e-milysol <milysol2702@gmail.com>
2026-09-03 10:44:46 +0200

    governance: fully restore sitemap to main

--- apps/web/src/pages/sitemap.xml.ts
+++ apps/web/src/pages/sitemap.xml.ts
@@ -6,7 +6,7 @@ const origin = 'https://peptibloomproject.com';
 const routes = ['/', '/about/', '/calculator/', '/library/', '/methodology/'];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes.map((route) => `  <url><loc>${origin}${route}</loc></url>`).join('\n')}
 </urlset>`;
 

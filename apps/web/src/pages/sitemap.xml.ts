41ac42327b1143a40742d4e59a34710bff226f39
e-milysol <milysol2702@gmail.com>
2026-09-03 11:23:22 +0200

    public-content: add COA reading guide to sitemap

--- apps/web/src/pages/sitemap.xml.ts
+++ apps/web/src/pages/sitemap.xml.ts
@@ -12,10 +12,11 @@ const routes = [
   '/research-basics/',
   '/research-basics/que-es-un-aminoacido/',
   '/research-basics/que-es-un-coa/',
+  '/research-basics/como-leer-un-coa/',
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/sitemap/0.9">
 ${routes.map((route) => `  <url><loc>${origin}${route}</loc></url>`).join('\n')}
 </urlset>`;
 
